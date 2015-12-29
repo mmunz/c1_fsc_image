501c1c2a05ddaa6fb1a57ade689dd4a758871ed6
Manuel Munz <munz@comuno.net>
2015-12-29 15:00:30 +0100

    Move dataprocessor to c1_fluid_styled_responsive_images

--- ext_tables.php
+++ ext_tables.php
@@ -12,10 +12,6 @@ if (!defined ('TYPO3_MODE')) {
 $pageTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TSConfig/page.ts');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTsConfig);
 
-// Add TCEFORM Config
-$tceformConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TSConfig/tceform.ts');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($tceformConfig);
-
 // register preview processor
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['image']
     = \C1\C1FscImage\Hooks\FscImagePreviewRenderer::class;
